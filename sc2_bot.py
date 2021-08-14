2e3e6df3b6ed56830e4d4bfc640a4870dba089f8
Deformater <76251290+Deformater@users.noreply.github.com>
2021-08-14 21:27:21 +0300

    Add files via upload

--- sc2_bot.py
+++ sc2_bot.py
@@ -4,11 +4,9 @@ import random
 import wikipedia
 from time import time
 
+from settings import *
 
-ADMIN_ID = 295354763
-GROUP_ID = 186771281
-TOKEN = 'd4239a6eb495308c8b826f40b8a10041e091f557ed149b1530e35ae2062af8404593a5768702043abfb25'
-CHAT_ID = 1
+
 with open('IDS.txt', 'r') as f:
     id_list = list(map(int, f.readlines()))
 
